130e20a261b314949bc1afaeee1c99204fe05a69
wataru okada <mail2wokada@gmail.com>
2022-12-23 11:46:10 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,100 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
-import * as trtc from "trtc-js-sdk"
+import TRTC, { Client } from "trtc-js-sdk"
 import { EXPIRETIME, SDKAPPID, SECRETKEY } from "./const";
 import "./index.css"
 
-
 const App = () => {
-    const signer = new window.LibGenerateTestUserSig(SDKAPPID, SECRETKEY, EXPIRETIME)
-    console.log(signer.genTestUserSig("aa"))
+
+
+    // useEffect(() => {
+    //     const testChecks = async () => {
+    //         const checkResult = await TRTC.checkSystemRequirements()
+    //         console.log(`Check System Requirements: ${JSON.stringify(checkResult)}`)
+    //         console.log(`Screen Share Support: ${TRTC.isScreenShareSupported()}`)
+    //         console.log(`Small Stream Support: ${TRTC.isSmallStreamSupported()}`)
+    //         console.log(`Cameras: ${JSON.stringify(TRTC.getCameras())}`)
+    //         console.log(`Microphones: ${JSON.stringify(TRTC.getMicrophones())}`)
+    //         console.log(`Speakers: ${JSON.stringify(TRTC.getSpeakers())}`)
+    //         console.log(`Devices: ${JSON.stringify(TRTC.getDevices())}`)
+    //     }
+    //     testChecks()
+    // }, [])
+
+    const clientRef = useRef<Client>()
+
+    const join = async () => {
+        const usernameInput = document.getElementById("username") as HTMLInputElement
+        const username = usernameInput.value
+        const roomIdInput = document.getElementById("room-id") as HTMLInputElement
+        const roomId = Number(roomIdInput.value)
+
+        const signer = new window.LibGenerateTestUserSig(SDKAPPID, SECRETKEY, EXPIRETIME)
+        const sign = signer.genTestUserSig(username)
+
+        clientRef.current = TRTC.createClient({
+            sdkAppId: SDKAPPID,
+            userId: username,
+            userSig: sign,
+            mode: 'rtc'
+        });
+
+        clientRef.current.on('stream-added', event => {
+            if (!clientRef.current) {
+                alert("client is not initialized.")
+                return
+            }
+            const remoteStream = event.stream;
+            const container = document.getElementById("remote-video-container") as HTMLDivElement
+            const divForRemote = document.createElement("div")
+            divForRemote.id = `remote-video-${remoteStream.getId()}`
+            container.appendChild(divForRemote)
+            clientRef.current.subscribe(remoteStream);
+        });
+        clientRef.current.on('stream-subscribed', event => {
+            const remoteStream = event.stream;
+            console.log('Subscribed to remote stream successfully:' + remoteStream.getId());
+            remoteStream.play(`remote-video-${remoteStream.getId()}`);
+        });
+
+        await clientRef.current.join({ roomId: roomId });
+        const localStream = TRTC.createStream({ userId: username, audio: true, video: true });
+        await localStream.initialize()
+        localStream.play('local-video-container');
+
+        await clientRef.current.publish(localStream);
+    }
+
+    const leave = async () => {
+        if (!clientRef.current) {
+            alert("client is not initialized.")
+            return
+        }
+        await clientRef.current.leave();
+        clientRef.current.destroy();
+    }
+
     return (
         <div className="root-div">
             <div className="header">
-                <div className="header-button" >load</div>
-                <div className="header-button" >load</div>
+                <div className="header-item-container">
+                    <div className="header-label">username</div>
+                    <input type="text" id="username"></input>
+                </div>
+                <div className="header-item-container">
+                    <div className="header-label">room id</div>
+                    <input type="number" id="room-id"></input>
+                </div>
+                <div className="header-item-container">
+                    <div className="header-button" onClick={join}>enter</div>
+                </div>
+                <div className="header-item-container">
+                    <div className="header-button" onClick={leave}>leave</div>
+                </div>
             </div>
             <div className="body">
-                <canvas id="test-canvas1" className="left-canvas"></canvas>
-                <canvas id="test-canvas2" className="right-canvas"></canvas>
+                <div id="remote-video-container"></div>
+                <div id="local-video-container"></div>
             </div>
         </div>
     )
